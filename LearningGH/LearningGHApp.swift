90863516c87c9a0d92356420441fb3c2afe0a83a
Ty Anderson <tyanderson2008@yahoo.com>
2023-07-26 17:21:19 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Tyrell Anderson on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
